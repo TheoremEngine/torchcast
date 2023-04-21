Significant update to criteria, including a number of broadcasting-related fixes

tests/nn_tests.py
@@ -10,6 +10,9 @@ class CriteriaTest(unittest.TestCase):
         with self.assertWarns(UserWarning):
             loss = tc.nn.L1Loss()(x_1, x_2).item()
         self.assertEqual(loss, 2.5)
+        with self.assertWarns(UserWarning):
+            loss = tc.nn.l1_loss(x_1, x_2).item()
+        self.assertEqual(loss, 2.5)
 
     def test_mse_loss(self):
         x_1 = torch.tensor([[1., 2., 3.], [float('nan'), -2., -3.]])
@@ -17,20 +20,24 @@ class CriteriaTest(unittest.TestCase):
         with self.assertWarns(UserWarning):
             loss = tc.nn.MSELoss()(x_1, x_2).item()
         self.assertEqual(loss, 6.5)
+        with self.assertWarns(UserWarning):
+            loss = tc.nn.mse_loss(x_1, x_2).item()
+        self.assertEqual(loss, 6.5)
 
     def test_soft_l1_loss(self):
-        criterion = tc.nn.SoftL1Loss()
         pred = torch.tensor([1., 2., 0.5, 2, -2])
         target = torch.tensor([1, 1, 0.25, 0.5, 0])
-        loss = criterion(pred, target)
+        loss = tc.nn.SoftL1Loss((0, 1))(pred, target)
+        self.assertEqual(loss, 1.75 / 5)
+        loss = tc.nn.soft_l1_loss(pred, target, (0, 1))
         self.assertEqual(loss, 1.75 / 5)
 
     def test_soft_mse_loss(self):
-        criterion = tc.nn.SoftMSELoss()
         pred = torch.tensor([1., 2., 0.5, 2, -2])
         target = torch.tensor([1, 1, 0.25, 0.5, 0])
-        loss = criterion(pred, target)
+        loss = tc.nn.SoftMSELoss((0, 1))(pred, target)
         self.assertEqual(loss, (0.25**2 + 1.5**2) / 5)
+        loss = tc.nn.soft_mse_loss(pred, target, (0, 1))
 
 
 class LayersTest(unittest.TestCase):

torchcast/nn/criteria.py
@@ -3,7 +3,49 @@ from typing import Tuple
 import torch
 import torch.nn.functional as f
 
-__all__ = ['L1Loss', 'MSELoss', 'SoftL1Loss', 'SoftMSELoss']
+__all__ = [
+    'l1_loss', 'L1Loss', 'mse_loss', 'MSELoss', 'soft_l1_loss', 'SoftL1Loss',
+    'soft_mse_loss', 'SoftMSELoss'
+]
+
+
+def _broadcast_mask_to_shape(mask: torch.Tensor, shape: torch.Size) \
+        -> torch.Tensor:
+    '''
+    This is a convenience function for broadcasting a mask to a specified
+    shape.
+
+    Args:
+        mask (:class:`torch.Tensor`): The mask to broadcast.
+        shape (:class:`torch.Size`): The shape to be broadcast to.
+    '''
+    if mask.ndim != len(shape):
+        raise ValueError(f'Shapes do not match: {mask.shape} vs {shape}')
+
+    for s, (d_m, d_s) in enumerate(zip(mask.shape, shape)):
+        if d_m == d_s:
+            continue
+        elif d_s == 1:
+            mask = mask.any(s, keepdim=True)
+        else:
+            raise ValueError(f'Shapes do not match: {mask.shape} vs {shape}')
+
+    return mask
+
+
+def l1_loss(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    '''
+    This is an L1 loss that ignores NaN values.
+    '''
+    is_real = ~(torch.isnan(pred) | torch.isnan(target))
+
+    if is_real.all():
+        return f.l1_loss(pred, target, reduction='mean')
+
+    pred_mask = _broadcast_mask_to_shape(is_real, pred.shape)
+    target_mask = _broadcast_mask_to_shape(is_real, target.shape)
+
+    return f.l1_loss(pred[pred_mask], target[target_mask], reduction='mean')
 
 
 class L1Loss(torch.nn.Module):
@@ -12,13 +54,22 @@ class L1Loss(torch.nn.Module):
     '''
     def forward(self, pred: torch.Tensor, target: torch.Tensor) \
             -> torch.Tensor:
-        is_real = ~(torch.isnan(pred) | torch.isnan(target))
-        # We apply the mask AFTER taking the loss so that predictions and
-        # targets can broadcast.
-        if is_real.all():
-            return f.l1_loss(pred, target, reduction='mean')
-        else:
-            return f.l1_loss(pred, target, reduction='none')[is_real].mean()
+        return l1_loss(pred, target)
+
+
+def mse_loss(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    '''
+    This is an MSE loss that ignores NaN values.
+    '''
+    is_real = ~(torch.isnan(pred) | torch.isnan(target))
+
+    if is_real.all():
+        return f.mse_loss(pred, target, reduction='mean')
+
+    pred_mask = _broadcast_mask_to_shape(is_real, pred.shape)
+    target_mask = _broadcast_mask_to_shape(is_real, target.shape)
+
+    return f.mse_loss(pred[pred_mask], target[target_mask], reduction='mean')
 
 
 class MSELoss(torch.nn.Module):
@@ -27,13 +78,29 @@ class MSELoss(torch.nn.Module):
     '''
     def forward(self, pred: torch.Tensor, target: torch.Tensor) \
             -> torch.Tensor:
-        is_real = ~(torch.isnan(pred) | torch.isnan(target))
-        # We apply the mask AFTER taking the loss so that predictions and
-        # targets can broadcast.
-        if is_real.all():
-            return f.mse_loss(pred, target, reduction='mean')
-        else:
-            return f.mse_loss(pred, target, reduction='none')[is_real].mean()
+        return mse_loss(pred, target)
+
+
+def soft_l1_loss(pred: torch.Tensor, target: torch.Tensor,
+                 target_range: Tuple[float, float] = (0., 1.)):
+    '''
+    This is an L1 loss that ignores when inputs go out of range in the same
+    direction as the target. For example, if the range of inputs is [0, 1], and
+    the target is 1, and the prediction is 2, this will produce a loss of 0.
+    However, if the target was 0, this would produce a loss of 2.
+    '''
+    target_min, target_max = target_range
+    target = torch.where(
+        torch.minimum(target, pred) >= target_max,
+        pred,
+        target
+    )
+    target = torch.where(
+        torch.maximum(target, pred) <= target_min,
+        pred,
+        target
+    )
+    return l1_loss(pred, target)
 
 
 class SoftL1Loss(torch.nn.Module):
@@ -45,19 +112,33 @@ class SoftL1Loss(torch.nn.Module):
     '''
     def __init__(self, target_range: Tuple[float, float] = (0, 1)):
         super().__init__()
-        self.target_min, self.target_max = target_range
+        self.target_range = target_range
 
     def forward(self, pred: torch.Tensor, target: torch.Tensor) \
             -> torch.Tensor:
-        target = torch.where(target < self.target_max, target, pred)
-        target = torch.where(target > self.target_min, target, pred)
-        is_real = ~(torch.isnan(pred) | torch.isnan(target))
-        # We apply the mask AFTER taking the loss so that predictions and
-        # targets can broadcast.
-        if is_real.all():
-            return f.l1_loss(pred, target, reduction='mean')
-        else:
-            return f.l1_loss(pred, target, reduction='none')[is_real].mean()
+        return soft_l1_loss(pred, target, self.target_range)
+
+
+def soft_mse_loss(pred: torch.Tensor, target: torch.Tensor,
+                  target_range: Tuple[float, float] = (0., 1.)):
+    '''
+    This is an MSE loss that ignores when inputs go out of range in the same
+    direction as the target. For example, if the range of inputs is [0, 1], and
+    the target is 1, and the prediction is 2, this will produce a loss of 0.
+    However, if the target was 0, this would produce a loss of 4.
+    '''
+    target_min, target_max = target_range
+    target = torch.where(
+        torch.minimum(target, pred) >= target_max,
+        pred,
+        target
+    )
+    target = torch.where(
+        torch.maximum(target, pred) <= target_min,
+        pred,
+        target
+    )
+    return mse_loss(pred, target)
 
 
 class SoftMSELoss(torch.nn.Module):
@@ -69,16 +150,8 @@ class SoftMSELoss(torch.nn.Module):
     '''
     def __init__(self, target_range: Tuple[float, float] = (0, 1)):
         super().__init__()
-        self.target_min, self.target_max = target_range
+        self.target_range = target_range
 
     def forward(self, pred: torch.Tensor, target: torch.Tensor) \
             -> torch.Tensor:
-        target = torch.where(target < self.target_max, target, pred)
-        target = torch.where(target > self.target_min, target, pred)
-        is_real = ~(torch.isnan(pred) | torch.isnan(target))
-        # We apply the mask AFTER taking the loss so that predictions and
-        # targets can broadcast.
-        if is_real.all():
-            return f.mse_loss(pred, target, reduction='mean')
-        else:
-            return f.mse_loss(pred, target, reduction='none')[is_real].mean()
+        return soft_mse_loss(pred, target, self.target_range)