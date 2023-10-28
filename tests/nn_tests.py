1a3f9f744ef64949a96001ef1a0c363346187c35
mlowell <75546495+Mark-Lowell@users.noreply.github.com>
2023-10-28 12:20:27 -0400

    Improved handling of tests when running without GPUs

--- tests/nn_tests.py
+++ tests/nn_tests.py
@@ -92,17 +92,18 @@ class ModelsTest(unittest.TestCase):
         net = tc.nn.EncoderDecoderTransformer(
             3, 32, 4, 2, 1, one_hot_encode_nan_inputs=True,
         )
-        net = net.cuda()
-        x = torch.randn((5, 3, 8), device='cuda')
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        net = net.to(device)
+        x = torch.randn((5, 3, 8), device=device)
         out = net(x, t_out=7)
         self.assertEqual(out.shape, (5, 4, 7))
-        t_in = torch.zeros((5, 1, 8), dtype=torch.int64, device='cuda')
+        t_in = torch.zeros((5, 1, 8), dtype=torch.int64, device=device)
         out = net(x, t_in=t_in, t_out=7)
         self.assertEqual(out.shape, (5, 4, 7))
-        t_in = torch.zeros((5, 1, 6), dtype=torch.int64, device='cuda')
+        t_in = torch.zeros((5, 1, 6), dtype=torch.int64, device=device)
         with self.assertRaises(ValueError):
             out = net(x, t_in=t_in, t_out=7)
-        t_out = torch.zeros((5, 1, 7), dtype=torch.int64, device='cuda')
+        t_out = torch.zeros((5, 1, 7), dtype=torch.int64, device=device)
         out = net(x, t_out=t_out)
         self.assertEqual(out.shape, (5, 4, 7))
 
@@ -110,15 +111,15 @@ class ModelsTest(unittest.TestCase):
         net = tc.nn.EncoderDecoderTransformer(
             3, 32, 4, 2, 1, exogenous_dim=7, one_hot_encode_nan_inputs=True,
         )
-        net = net.cuda()
-        x_in = torch.randn((5, 3, 8), device='cuda')
-        exog_in = torch.randn((5, 7, 8), device='cuda')
-        exog_out = torch.randn((5, 7, 7), device='cuda')
+        net = net.to(device)
+        x_in = torch.randn((5, 3, 8), device=device)
+        exog_in = torch.randn((5, 7, 8), device=device)
+        exog_out = torch.randn((5, 7, 7), device=device)
         out = net(x_in, exog_out=exog_out, exog_in=exog_in)
         self.assertEqual(out.shape, (5, 4, 7))
         out = net(x_in, exog_out=exog_out, t_out=7, exog_in=exog_in)
         self.assertEqual(out.shape, (5, 4, 7))
-        t_in = torch.zeros((5, 1, 8), dtype=torch.int64, device='cuda')
+        t_in = torch.zeros((5, 1, 8), dtype=torch.int64, device=device)
         out = net(x_in, exog_out=exog_out, exog_in=exog_in, t_in=t_in, t_out=7)
         self.assertEqual(out.shape, (5, 4, 7))
         with self.assertRaises(ValueError):
@@ -130,18 +131,19 @@ class ModelsTest(unittest.TestCase):
         net = tc.nn.EncoderTransformer(
             3, 32, 4, 2, num_classes=10, one_hot_encode_nan_inputs=True,
         )
-        net = net.cuda()
-        x = torch.randn((5, 3, 8), device='cuda')
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        net = net.to(device)
+        x = torch.randn((5, 3, 8), device=device)
         cls_pred, out = net(x, t_out=7)
         self.assertEqual(cls_pred.shape, (5, 10))
         self.assertEqual(out.shape, (5, 4, 7))
-        t_in = torch.zeros((5, 1, 8), dtype=torch.int64, device='cuda')
+        t_in = torch.zeros((5, 1, 8), dtype=torch.int64, device=device)
         _, out = net(x, t_in=t_in, t_out=7)
         self.assertEqual(out.shape, (5, 4, 7))
-        t_in = torch.zeros((5, 1, 6), dtype=torch.int64, device='cuda')
+        t_in = torch.zeros((5, 1, 6), dtype=torch.int64, device=device)
         with self.assertRaises(ValueError):
             net(x, t_in=t_in, t_out=7)
-        t_out = torch.zeros((5, 1, 7), dtype=torch.int64, device='cuda')
+        t_out = torch.zeros((5, 1, 7), dtype=torch.int64, device=device)
         _, out = net(x, t_out=t_out)
         self.assertEqual(out.shape, (5, 4, 7))
 
@@ -149,15 +151,15 @@ class ModelsTest(unittest.TestCase):
         net = tc.nn.EncoderTransformer(
             3, 32, 4, 2, exogenous_dim=7, one_hot_encode_nan_inputs=True,
         )
-        net = net.cuda()
-        x_in = torch.randn((5, 3, 8), device='cuda')
-        exog_in = torch.randn((5, 7, 8), device='cuda')
-        exog_out = torch.randn((5, 7, 7), device='cuda')
+        net = net.to(device)
+        x_in = torch.randn((5, 3, 8), device=device)
+        exog_in = torch.randn((5, 7, 8), device=device)
+        exog_out = torch.randn((5, 7, 7), device=device)
         out = net(x_in, exog_out=exog_out, exog_in=exog_in)
         self.assertEqual(out.shape, (5, 4, 7))
         out = net(x_in, exog_out=exog_out, exog_in=exog_in, t_out=7)
         self.assertEqual(out.shape, (5, 4, 7))
-        t_in = torch.zeros((5, 1, 8), dtype=torch.int64, device='cuda')
+        t_in = torch.zeros((5, 1, 8), dtype=torch.int64, device=device)
         out = net(x_in, exog_out=exog_out, exog_in=exog_in, t_in=t_in, t_out=7)
         self.assertEqual(out.shape, (5, 4, 7))
         with self.assertRaises(ValueError):
