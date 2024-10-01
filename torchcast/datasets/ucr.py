e63a6610a71499cea4785177cd85ebe2d72d4b23
Mark Lowell <MarkLowell@theorem-engine.org>
2024-10-01 18:53:02 -0400

    Changed UCR fetchers to point to zenodo.org instead of timeseriesclassification.com

--- torchcast/datasets/ucr.py
+++ torchcast/datasets/ucr.py
@@ -8,56 +8,82 @@ from .utils import _download_and_extract
 
 __all__ = ['UCRDataset', 'UEADataset']
 
-UCR_URL = 'http://www.timeseriesclassification.com/aeon-toolkit/Archives/Univariate2018_ts.zip'  # noqa: E501
+ZENODO_URL = 'https://zenodo.org/record/{zenodo_id}/files/{name}'
 
-UEA_URL = 'http://www.timeseriesclassification.com/aeon-toolkit/Archives/Multivariate2018_ts.zip'  # noqa: E501
+UCR_DATASETS = {
+    'ACSF1': 11184893, 'Adiac': 11179788, 'AllGestureWiimoteX': 11185036,
+    'AllGestureWiimoteY': 11185107, 'AllGestureWiimoteZ': 11185136,
+    'ArrowHead': 11185163, 'Beef': 11185190, 'BeetleFly': 11185218,
+    'BirdChicken': 11185259, 'BME': 11185291, 'Car': 11185322, 'CBF': 11186181,
+    'Chinatown': 11186207, 'ChlorineConcentration': 11186229,
+    'CinCECGTorso': 11186247, 'Coffee': 11186266, 'Computers': 11186293,
+    'CricketX': 11186304, 'CricketY': 11186320, 'CricketZ': 11186333,
+    'Crop': 11186344, 'DiatomSizeReduction': 11186365,
+    'DistalPhalanxOutlineAgeGroup': 11186386,
+    'DistalPhalanxOutlineCorrect': 11186597, 'DistalPhalanxTW': 11186610,
+    'DodgerLoopDay': 11186618, 'DodgerLoopGame': 11186628,
+    'DodgerLoopWeekend': 11186647, 'Earthquakes': 11186659, 'ECG200': 11186675,
+    'ECG5000': 11186692, 'ECGFiveDays': 11186702, 'ElectricDevices': 11190880,
+    'EOGHorizontalSignal': 11190930, 'EOGVerticalSignal': 11190951,
+    'EthanolLevel': 11190985, 'FaceAll': 11191011, 'FaceFour': 11191042,
+    'FacesUCR': 11191065, 'FiftyWords': 11191097, 'Fish': 11191141,
+    'FordA': 11191164, 'FordB': 11191172, 'FreezerRegularTrain': 11191184,
+    'FreezerSmallTrain': 11191211, 'Fungi': 11191230,
+    'GestureMidAirD1': 11197478, 'GestureMidAirD2': 11197490,
+    'GestureMidAirD3': 11197504, 'GesturePebbleZ1': 11197515,
+    'GesturePebbleZ2': 11197520, 'GunPoint': 11191244,
+    'GunPointAgeSpan': 11194425, 'GunPointMaleVersusFemale': 11194429,
+    'GunPointOldVersusYoung': 11194437, 'Ham': 11197526,
+    'HandOutlines': 11197528, 'Haptics': 11197538, 'Herring': 11197540,
+    'HouseTwenty': 11197555, 'InlineSkate': 11197575,
+    'InsectEPGRegularTrain': 11197587, 'InsectEPGSmallTrain': 11197608,
+    'InsectWingbeatSound': 11197635, 'ItalyPowerDemand': 11197656,
+    'LargeKitchenAppliances': 11197689, 'Lightning2': 11197697,
+    'Lightning7': 11197706, 'Mallat': 11197731, 'Meat': 11197742,
+    'MedicalImages': 11197752, 'MelbournePedestrian': 11197762,
+    'MiddlePhalanxOutlineAgeGroup': 11197771,
+    'MiddlePhalanxOutlineCorrect': 11197782, 'MiddlePhalanxTW': 11197799,
+    'MixedShapesRegularTrain': 11197803, 'MixedShapesSmallTrain': 11197811,
+    'MoteStrain': 11197817, 'NonInvasiveFetalECGThorax1': 11197817,
+    'NonInvasiveFetalECGThorax2': 11197831, 'OliveOil': 11197843,
+    'OSULeaf': 11197848, 'PhalangesOutlinesCorrect': 11197875,
+    'Phoneme': 11197891, 'PickupGestureWiimoteZ': 11197898,
+    'PigAirwayPressure': 11197911, 'PigArtPressure': 11197920,
+    'PigCVP': 11197924, 'PLAID': 11197936, 'Plane': 11197940,
+    'PowerCons': 11197948, 'ProximalPhalanxOutlineAgeGroup': 11197960,
+    'ProximalPhalanxOutlineCorrect': 11197968, 'ProximalPhalanxTW': 11197973,
+    'RefrigerationDevices': 11197996, 'Rock': 11198001, 'ScreenType': 11198182,
+    'SemgHandGenderCh2': 11198193, 'SemgHandMovementCh2': 11198197,
+    'SemgHandSubjectCh2': 11198203, 'ShakeGestureWiimoteZ': 11198219,
+    'ShapeletSim': 11198235, 'ShapesAll': 11198237,
+    'SmallKitchenAppliances': 11198251, 'SmoothSubspace': 11198271,
+    'SonyAIBORobotSurface1': 11198277, 'SonyAIBORobotSurface2': 11198290,
+    'StarLightCurves': 11198308, 'Strawberry': 11198313,
+    'SwedishLeaf': 11198315, 'Symbols': 11198322, 'SyntheticControl': 11198330,
+    'ToeSegmentation1': 11198338, 'ToeSegmentation2': 11198342,
+    'Trace': 11198344, 'TwoLeadECG': 11198352, 'TwoPatterns': 11198356,
+    'UMD': 11198362, 'UWaveGestureLibraryAll': 11198366,
+    'UWaveGestureLibraryX': 11198374, 'UWaveGestureLibraryY': 11198382,
+    'UWaveGestureLibraryZ': 11198384, 'Wafer': 11198387, 'Wine': 11198391,
+    'WordSynonyms': 11198396, 'Worms': 11198402, 'WormsTwoClass': 11198406,
+    'Yoga': 11198408,
+}
 
-UCR_DATASETS = (
-    'ACSF1', 'Adiac', 'AllGestureWiimoteX', 'AllGestureWiimoteY',
-    'AllGestureWiimoteZ', 'ArrowHead', 'Beef', 'BeetleFly', 'BirdChicken',
-    'BME', 'Car', 'CBF', 'Chinatown', 'ChlorineConcentration', 'CinCECGTorso',
-    'Coffee', 'Computers', 'CricketX', 'CricketY', 'CricketZ', 'Crop',
-    'DiatomSizeReduction', 'DistalPhalanxOutlineAgeGroup',
-    'DistalPhalanxOutlineCorrect', 'DistalPhalanxTW', 'DodgerLoopDay',
-    'DodgerLoopGame', 'DodgerLoopWeekend', 'Earthquakes', 'ECG200', 'ECG5000',
-    'ECGFiveDays', 'ElectricDevices', 'EOGHorizontalSignal',
-    'EOGVerticalSignal', 'EthanolLevel', 'FaceAll', 'FaceFour', 'FacesUCR',
-    'FiftyWords', 'Fish', 'FordA', 'FordB', 'FreezerRegularTrain',
-    'FreezerSmallTrain', 'Fungi', 'GestureMidAirD1', 'GestureMidAirD2',
-    'GestureMidAirD3', 'GesturePebbleZ1', 'GesturePebbleZ2', 'GunPoint',
-    'GunPointAgeSpan', 'GunPointMaleVersusFemale', 'GunPointOldVersusYoung',
-    'Ham', 'HandOutlines', 'Haptics', 'Herring', 'HouseTwenty', 'InlineSkate',
-    'InsectEPGRegularTrain', 'InsectEPGSmallTrain', 'InsectWingbeatSound',
-    'ItalyPowerDemand', 'LargeKitchenAppliances', 'Lightning2', 'Lightning7',
-    'Mallat', 'Meat', 'MedicalImages', 'MelbournePedestrian',
-    'MiddlePhalanxOutlineAgeGroup', 'MiddlePhalanxOutlineCorrect',
-    'MiddlePhalanxTW', 'MixedShapesRegularTrain', 'MixedShapesSmallTrain',
-    'MoteStrain', 'NonInvasiveFetalECGThorax1', 'NonInvasiveFetalECGThorax2',
-    'OliveOil', 'OSULeaf', 'PhalangesOutlinesCorrect', 'Phoneme',
-    'PickupGestureWiimoteZ', 'PigAirwayPressure', 'PigArtPressure', 'PigCVP',
-    'PLAID', 'Plane', 'PowerCons', 'ProximalPhalanxOutlineAgeGroup',
-    'ProximalPhalanxOutlineCorrect', 'ProximalPhalanxTW',
-    'RefrigerationDevices', 'Rock', 'ScreenType', 'SemgHandGenderCh2',
-    'SemgHandMovementCh2', 'SemgHandSubjectCh2', 'ShakeGestureWiimoteZ',
-    'ShapeletSim', 'ShapesAll', 'SmallKitchenAppliances', 'SmoothSubspace',
-    'SonyAIBORobotSurface1', 'SonyAIBORobotSurface2', 'StarLightCurves',
-    'Strawberry', 'SwedishLeaf', 'Symbols', 'SyntheticControl',
-    'ToeSegmentation1', 'ToeSegmentation2', 'Trace', 'TwoLeadECG',
-    'TwoPatterns', 'UMD', 'UWaveGestureLibraryAll', 'UWaveGestureLibraryX',
-    'UWaveGestureLibraryY', 'UWaveGestureLibraryZ', 'Wafer', 'Wine',
-    'WordSynonyms', 'Worms', 'WormsTwoClass', 'Yoga'
-)
-
-UEA_DATASETS = (
-    'ArticularyWordRecognition', 'AtrialFibrillation', 'BasicMotions',
-    'CharacterTrajectories', 'Cricket', 'DuckDuckGeese', 'EigenWorms',
-    'Epilepsy', 'ERing', 'EthanolConcentration', 'FaceDetection',
-    'FingerMovements', 'HandMovementDirection', 'Handwriting', 'Heartbeat',
-    'InsectWingbeat', 'JapaneseVowels', 'Libras', 'LSST', 'MotorImagery',
-    'NATOPS', 'PEMS-SF', 'PenDigits', 'PhonemeSpectra', 'RacketSports',
-    'SelfRegulationSCP1', 'SelfRegulationSCP2', 'SpokenArabicDigits',
-    'StandWalkJump', 'UWaveGestureLibrary',
-)
+UEA_DATASETS = {
+    'ArticularyWordRecognition': 11204924, 'AtrialFibrillation': 11206175,
+    'BasicMotions': 11206179, 'CharacterTrajectories': 11206183,
+    'Cricket': 11206185, 'DuckDuckGeese': 11206189, 'EigenWorms': 11206196,
+    'Epilepsy': 11206204, 'EthanolConcentration': 11206212, 'ERing': 11206210,
+    'FaceDetection': 11206216, 'FingerMovements': 11206220,
+    'HandMovementDirection': 11206224, 'Handwriting': 11206227,
+    'Heartbeat': 11206229, 'InsectWingbeat': 11206234,
+    'JapaneseVowels': 11206237, 'Libras': 11206239, 'LSST': 11206243,
+    'MotorImagery': 11206246, 'NATOPS': 11206248, 'PenDigits': 11206259,
+    'PEMS-SF': 11206252, 'PhonemeSpectra': 11206261, 'RacketSports': 11206263,
+    'SelfRegulationSCP1': 11206265, 'SelfRegulationSCP2': 11206269,
+    'SpokenArabicDigits': 11206274, 'StandWalkJump': 11206278,
+    'UWaveGestureLibrary': 11206282,
+}
 
 
 class UCRDataset(TensorSeriesDataset):
@@ -88,16 +114,18 @@ class UCRDataset(TensorSeriesDataset):
                 sequence to return. If not provided, returns an entire
                 sequence.
         '''
-        if task not in UCR_DATASETS:
+        if task not in UCR_DATASETS.keys():
             raise ValueError(
-                f'Did not recognize {task}; choose from {UCR_DATASETS}'
+                f'Did not recognize {task}; choose from '
+                f'{tuple(UCR_DATASETS.keys())}'
             )
         if split not in {'train', 'test'}:
             raise ValueError(f"Split should be 'train' or 'test', got {split}")
 
+        file_name = f'{task}_{split.upper()}.ts'
         buff = _download_and_extract(
-            UCR_URL,
-            f'Univariate_ts/{task}/{task}_{split.upper()}.ts',
+            ZENODO_URL.format(zenodo_id=UCR_DATASETS[task], name=file_name),
+            file_name,
             path,
             download=download,
         )
@@ -143,16 +171,18 @@ class UEADataset(TensorSeriesDataset):
                 sequence to return. If not provided, returns an entire
                 sequence.
         '''
-        if task not in UEA_DATASETS:
+        if task not in UEA_DATASETS.keys():
             raise ValueError(
-                f'Did not recognize {task}; choose from {UEA_DATASETS}'
+                f'Did not recognize {task}; choose from '
+                f'{tuple(UEA_DATASETS.keys())}'
             )
         if split not in {'train', 'test'}:
             raise ValueError(f"Split should be 'train' or 'test', got {split}")
 
+        file_name = f'{task}_{split.upper()}.ts'
         buff = _download_and_extract(
-            UEA_URL,
-            f'Multivariate_ts/{task}/{task}_{split.upper()}.ts',
+            ZENODO_URL.format(zenodo_id=UEA_DATASETS[task], name=file_name),
+            file_name,
             path,
             download=download,
         )
