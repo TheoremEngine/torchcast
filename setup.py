6c898a069bd6a185b5a03eaeed2ce0e0dc9009d7
Mark Lowell <MarkLowell@theorem-engine.org>
2025-02-24 18:54:00 -0500

    Fixed build system

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import os
 from setuptools import find_packages, setup
 
 from pybind11.setup_helpers import Pybind11Extension
@@ -9,17 +8,16 @@ COMPILE_ARGS = [
     '-Wsign-conversion',
 ]
 
+REQUIREMENTS = [
+    'h5py',
+    'pandas',
+    'pybind11',
+    'requests',
+    'torch',
+]
+
 
 if __name__ == '__main__':
-    # Hypothetically, somebody might run this from somewhere other than the
-    # root directory.
-    root_path = os.path.dirname(os.path.abspath(__file__))
-
-    # Read in requirements.txt
-    req_path = os.path.join(root_path, 'requirements.txt')
-    with open(req_path, 'r') as reqs_file:
-        requirements = list(reqs_file.readlines())
-
     # Create pybind11 extension
     ext_modules = [
         Pybind11Extension(
@@ -40,6 +38,6 @@ if __name__ == '__main__':
         author_email='MarkLowell@theorem-engine.org',
         packages=find_packages(),
         package_data={'torchcast': ['datasets/*.txt']},
-        install_requires=requirements,
+        install_requires=REQUIREMENTS,
         ext_modules=ext_modules,
     )
