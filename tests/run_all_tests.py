203bc4bcab9050394ed01e11ca57c5ebebd9812e
Mark Lowell <MarkLowell@theorem-engine.org>
2025-02-25 19:29:07 -0500

    Fixed issue during testing, where warnings emitted during imports cause an error

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -1,13 +1,15 @@
 import unittest
 import warnings
 
-warnings.simplefilter('error')
-
 from data_tests import *
 from dataset_tests import *
 from ltsf_tests import *
 from nn_tests import *
 from utils_tests import *
+
+# This needs to go *after* the imports, because certain dependencies can emit
+# warnings on import under some circumstances.
+warnings.simplefilter('error')
 
 
 if __name__ == '__main__':
