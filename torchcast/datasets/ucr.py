c54496d1a6f02f00bb8f85a82aae2744168a0da5
Mark Lowell <MarkLowell@theorem-engine.org>
2026-04-30 17:39:24 -0400

    Added expansion to UEA

--- torchcast/datasets/ucr.py
+++ torchcast/datasets/ucr.py
@@ -69,20 +69,70 @@ UCR_DATASETS = {
     'Yoga': 11198408,
 }
 
-UEA_DATASETS = {
-    'ArticularyWordRecognition': 11204924, 'AtrialFibrillation': 11206175,
-    'BasicMotions': 11206179, 'CharacterTrajectories': 11206183,
-    'Cricket': 11206185, 'DuckDuckGeese': 11206189, 'EigenWorms': 11206196,
-    'Epilepsy': 11206204, 'EthanolConcentration': 11206212, 'ERing': 11206210,
-    'FaceDetection': 11206216, 'FingerMovements': 11206220,
-    'HandMovementDirection': 11206224, 'Handwriting': 11206227,
-    'Heartbeat': 11206229, 'InsectWingbeat': 11206234,
-    'JapaneseVowels': 11206237, 'Libras': 11206239, 'LSST': 11206243,
-    'MotorImagery': 11206246, 'NATOPS': 11206248, 'PenDigits': 11206259,
-    'PEMS-SF': 11206252, 'PhonemeSpectra': 11206261, 'RacketSports': 11206263,
-    'SelfRegulationSCP1': 11206265, 'SelfRegulationSCP2': 11206269,
-    'SpokenArabicDigits': 11206274, 'StandWalkJump': 11206278,
-    'UWaveGestureLibrary': 11206282,
+UEA_DATASET = {
+    'Alzheimers': 15500801, 'AppliancesEnergy_disc': 18497240,
+    'ArticularyWordRecognition': 11204924,
+    'AsphaltObstaclesCoordinates': 18497270,
+    'AsphaltPavementTypeCoordinates': 18497280,
+    'AsphaltRegularityCoordinates': 18497298, 'AtrialFibrillation': 11206175,
+    'AustraliaRainfall_disc': 18497322, 'AutomotiveRoadTrials': 18497336,
+    'BIDMC32HR_disc': 18497352, 'BIDMC32RR_disc': 18497425,
+    'BIDMC32SpO2_disc': 18497447, 'BasicMotions': 11206179,
+    'BeijingPM10Quality_disc': 18497561, 'BeijingPM25Quality_disc': 18498242,
+    'BenzeneConcentration_disc': 18498580, 'Blink': 18735667,
+    'BoneIntensitiesAgeGroup': 18498586, 'BoneProbAgeGroup': 18498595,
+    'ButtonPress': 15522930, 'CharacterTrajectories': 18723007,
+    'CounterMovementJump': 18498603, 'Cricket': 11206185, 
+    'CrowdSourced': 18498611, 'DREAMERA': 18498618, 'DREAMERV': 18498620,
+    'DuckDuckGeese': 11206189, 'ERing': 11206210, 'EigenWorms': 11206196,
+    'EmoPain': 18499143, 'Epilepsy': 11206204,
+    'EthanolConcentration': 11206212, 'EyesOpenShut': 18496872,
+    'FaceDetection': 11206216, 'FeedbackButton': 18957700,
+    'FeetHands': 15495750, 'FingerMovements': 11206220,
+    'FordChallenge': 18499147, 'HandMovementDirection': 11206224,
+    'Handwriting': 11206227, 'Heartbeat': 11206229,
+    'HouseholdPowerConsumption1_disc': 18499150,
+    'HouseholdPowerConsumption2_disc': 18499156, 'IEEEPPG_disc': 18499163,
+    'IRDS-EFL': 18510498, 'IRDS-EFR': 18510501, 'IRDS-SAL': 18510505,
+    'IRDS-SAR': 18510507, 'IRDS-SFE': 18510509, 'IRDS-SFL': 18510515,
+    'IRDS-SFR': 18510518, 'IRDS-STL': 18510520, 'IRDS-STR': 18510522,
+    'ImaginedFeetHands': 15493041, 'ImaginedOpenCloseFist': 15493010,
+    'InnerSpeech': 15425020, 'InsectWingbeat': 11206234,
+    'JapaneseVowels': 18735628, 'KERAAL-CTK': 18510796,
+    'KERAAL-CTK-MC': 18510800, 'KERAAL-ELK': 18510806,
+    'KERAAL-ELK-MC': 18510811, 'KERAAL-RTK': 18510813,
+    'KERAAL-RTK-MC': 18510817, 'KIMORE-LA-C': 18510879,
+    'KIMORE-LT-C': 18510884, 'KIMORE-PR-C': 18510887,
+    'KIMORE-Sq-C': 18510889, 'KIMORE-TR-C': 18510893,
+    'KINECAL-3WFV': 18510895, 'KINECAL-GGFV': 18510901,
+    'KINECAL-QSEC': 18510903, 'KINECAL-QSEO': 18510905,
+    'LSST': 11206243, 'LenDB': 18499169, 'Libras': 11206239,
+    'LiveFuelMoistureContent_disc': 18497210, 'Locust2022': 18499215,
+    'LongIntervalTask': 15427724, 'LowCost': 15523038,
+    'MatchingPennies': 15523058, 'MindReading': 15523138,
+    'MotionSenseHAR': 18499225, 'MotorImagery': 11206246,
+    'NATOPS': 11206248, 'NewsHeadlineSentiment_disc': 18499242,
+    'NewsTitleSentiment_disc': 18499244, 'OpenCloseFist': 15492985,
+    'Opportunity': 18499292, 'PAMAP2': 18499363, 'PEMS-SF': 11206252,
+    'PPGDalia_disc': 18499660, 'PenDigits': 11206259,
+    'PhonemeSpectra': 11206261, 'PhotoStimulation': 15500781,
+    'PronouncedSpeech': 15392755, 'RacketSports': 11206263,
+    'S2Agri-10pc-17': 18499796, 'S2Agri-10pc-34': 18499800,
+    'S2Agri-17': 18505001, 'S2Agri-34': 18504996, 'SPHERE-WUS': 18510936,
+    'STEW': 18500110, 'SelfRegulationSCP1': 11206265,
+    'SelfRegulationSCP2': 11206269, 'ShortIntervalTask': 15491177,
+    'SitStand': 15500726, 'Skoda': 18500184, 'SongFamiliarity': 15496080,
+    'SpokenArabicDigits': 18734026, 'StandWalkJump': 11206278,
+    'TactileTextureRecognition': 18500187, 'Tiselac': 18500191,
+    'UCDHE-MP': 18510938, 'UCDHE-MP-MC': 18510940, 'UCDHE-Rowing': 18510942,
+    'UCDHE-Rowing-MC': 18510948, 'UCIActivity': 18500267,
+    'UIPRMD-DS-C': 18511325, 'UIPRMD-HS-C': 18511341, 'UIPRMD-IL-C': 18511343,
+    'UIPRMD-SASLR-C': 18511347, 'UIPRMD-SL-C': 18511349,
+    'UIPRMD-SSA-C': 18511351, 'UIPRMD-SSE-C': 18511355,
+    'UIPRMD-SSIER-C': 18511357, 'UIPRMD-SSS-C': 18511361,
+    'UIPRMD-STS-C': 18511363, 'USCActivity': 18500265,
+    'UWaveGestureLibrary': 11206282, 'VisualSpeech': 15366803,
+    'WISDM': 18500261, 'WISDM2': 18500263
 }
 
 
@@ -151,6 +201,11 @@ class UEADataset(TensorSeriesDataset):
 
         https://www.timeseriesclassification.com/
 
+    This includes the additional datasets introduced by:
+
+        Middlehurst et al. 2026. ``The Multiverse of Time Series Machine
+        Learning: an Archive for Multivariate Time Series Classification.''
+        `arXiv: 2603.20352. <https://arxiv.org/abs/2603.20352>`__
     '''
     tasks: List[str] = list(UEA_DATASETS)
 
